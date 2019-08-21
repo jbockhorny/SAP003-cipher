387990d9e6bb0fbac5df05355bfecd65e7beb5d3
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-08-21 13:50:21 -0300

    add função de cifrar e botão

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,16 @@
 window.cipher = {
-  // ... 
-};
+     
+     
+       // Como codificar uma letra     
+
+       //function codificarLetra(letra, deslocamento){
+                                 
+        //let tamDoAlfabeto = 26;
+        //let codAsc = 65;
+
+        //let letraCodificada = ((letra.charCodeAt(0)-codAsc)+deslocamento)% tamDoAlfabeto +codAsc;
+        //return String.fromCharCode(letraCodificada);
+            
+    //}  
+       
+};      
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Pegar o conteúdo digitado pelo usuário
+       
+       
+function pegarValores(){
+
+       
+    let mensagem = document.getElementById("mensagem").value;
+    console.log(mensagem);
+
+    let offset = document.getElementById("offset").value;
+    console.log(offset);
+
+   }
+
+//while(" ") {
+
+    //onclick
+
+    //alert("Para codificar digite uma mensagem");
+//}  
