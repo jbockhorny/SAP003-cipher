4335e18c6cc575cfdd503bc182f5859c072f1392
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-08-26 14:53:51 -0300

    Corrigindo o botão

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-function takeValueEncode() {
+document.getElementById("codificar").addEventListener("click", takeValueEncode);
 
+function takeValueEncode() {
+  event.preventDefault();
+  
   let text = document.getElementById("text").value;
   let offset = Number(document.getElementById("offset").value);
            
   if ( text === "" || isNaN(offset) || offset === 0 ) {
     alert("Para codificar digite uma mensagem e o deslocamento!");
 
-  } else if (offset <0){
+  } else if (offset <0) {
     document.getElementById("Resultado").innerHTML=`<p>A mensagem codificada é ${window.cipher.decode(text, -offset)}</p>`;
 
-  }else {        
+  } else {        
     document.getElementById("Resultado").innerHTML=`<p>A mensagem codificada é ${window.cipher.encode(text, offset)}</p>`;
   }
 }
 
+document.getElementById("decodificar").addEventListener("click", takeValueDecode);
 function takeValueDecode() {
+  event.preventDefault();
 
   let text = document.getElementById("text").value;
   let offset = Number(document.getElementById("offset").value);
@@ -22,7 +27,7 @@ function takeValueDecode() {
   if ( text === "" || isNaN(offset)|| offset === 0 ) {
     alert("Para decodificar digite uma mensagem e o deslocamento!");
 
-  } else if (offset <0){
+  } else if (offset <0) {
     document.getElementById("Resultado").innerHTML=`<p>A mensagem decodificada é ${window.cipher.encode(text, -offset)}</p>`;
 
   } else {
