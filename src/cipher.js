6e96a1e97a8abef2dab0924ab98991cddeea2609
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-11-11 10:08:13 -0300

    refatoração

--- src/cipher.js
+++ src/cipher.js
@@ -3,56 +3,53 @@ window.cipher = {
   decode: decode
 };
 
+let alphabetSize = 26;
+// offset > 0 ? alphabetSize = 26 : alphabetSize = 25;
+
 function encode(offset, text) {
 
-  let textEncode =[]; 
-  let alphabetSize = 26;
+  let textEncode;
   let firstAsciUppercase = 65;
   let firstAsciLowercase = 97;
 
-  for (let i=0; i < text.length; i++) { 
+  for (let i = 0; i < text.length; i++) {
 
-    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90) {
+    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
 
-      textEncode[i]= (text.charCodeAt(i)-firstAsciUppercase+offset)% alphabetSize +firstAsciUppercase;
-      textEncode[i]= (String.fromCharCode(textEncode[i]));
-                                                                
-    } else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122) {
+      textEncode += String.fromCharCode((text.charCodeAt(i) - firstAsciUppercase + offset) % alphabetSize + firstAsciUppercase);
 
-      textEncode[i]= (text.charCodeAt(i)-firstAsciLowercase+offset)% alphabetSize +firstAsciLowercase;
-      textEncode[i]= (String.fromCharCode(textEncode[i]));                           
-                  
+
+    } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+
+      textEncode += String.fromCharCode((text.charCodeAt(i) - firstAsciLowercase + offset) % alphabetSize + firstAsciLowercase);
+
     } else {
-      textEncode[i] = text.charAt(i);
-    }                  
-                      
-  } 
-  return textEncode.join(""); 
-    
-}     
+      textEncode += text.charAt(i);
+    }
+  }
+  return textEncode;
+
+}
 
 function decode(offset, text) {
-		
-  let textDecode =[];    
-  let alphabetSize = 26;
+
+  let textDecode;
   let firstAsciUppercase = 90;
   let firstAsciLowercase = 122;
 
-  for (let i=0; i < text.length; i++) { 
+  for (let i = 0; i < text.length; i++) {
 
-    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90) {
+    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
 
-      textDecode[i] = (text.charCodeAt(i)-firstAsciUppercase-offset)% alphabetSize +firstAsciUppercase;
-      textDecode[i] = (String.fromCharCode(textDecode[i]));
-                          
-    } else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122) {
+      textDecode += String.fromCharCode((text.charCodeAt(i) - firstAsciUppercase - offset) % alphabetSize + firstAsciUppercase);
 
-      textDecode[i]= (text.charCodeAt(i)-firstAsciLowercase-offset)% alphabetSize +firstAsciLowercase;
-      textDecode[i]= (String.fromCharCode(textDecode[i]));                
-              
+    } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+
+      textDecode += String.fromCharCode((text.charCodeAt(i) - firstAsciLowercase - offset) % alphabetSize + firstAsciLowercase);
+
     } else {
-      textDecode[i] = text.charAt(i);
-    }                 
+      textDecode += text.charAt(i);
+    }
   }
-  return textDecode.join("");          
+  return textDecode;
 }
--- src/index.js
+++ src/index.js
@@ -3,45 +3,40 @@ document.getElementById("encode").addEventListener("click", takeValueEncode);
 let form = document.getElementById("form");
 let result = document.getElementById("result");
 
-function takeValueEncode() {
+function takeValueEncode(event) {
   event.preventDefault();
 
   let offset = Number(document.getElementById("offset").value);
   let text = document.getElementById("text").value;
            
-  if ( text === "" || isNaN(offset) || offset === 0 ) {
+  if ( !text || !offset ) {
     alert("Para codificar digite uma mensagem e o deslocamento!");    
 
   } else if ((offset %26) === 0) { 
     alert("Com o deslocamento " + offset + " você percorreu o alfabeto inteiro e voltou a sua mensagem original!");
-  
-  } else if (offset <0) {
-    result.innerHTML=`<p>A mensagem "${text}" codificada é: ${window.cipher.decode(-offset, text)}!</p>`;
 
   } else {        
-    result.innerHTML=`<p>A mensagem "${text}"codificada é ${window.cipher.encode(offset, text)}!</p>`;
+    result.textContent=`<p>A mensagem "${text}"codificada é ${window.cipher.encode(offset, text)}!</p>`;
   }
   form.reset(); 
 }
 
 document.getElementById("decode").addEventListener("click", takeValueDecode);
-function takeValueDecode() {
+
+function takeValueDecode(event) {
   event.preventDefault();
 
   let offset = Number(document.getElementById("offset").value);
   let text = document.getElementById("text").value;
           
-  if ( text === "" || isNaN(offset)|| offset === 0 ) {
+  if ( !text || !offset ) {
     alert("Para decodificar digite uma mensagem e o deslocamento!");
 
   } else if ((offset %26) === 0) { 
     alert("Com o deslocamento " + offset + " você percorreu o alfabeto inteiro e voltou a sua mensagem original!");
 
-  } else if (offset <0) {
-    result.innerHTML=`<p>A mensagem "${text}" decodificada é ${window.cipher.encode(-offset, text)}!</p>`;
-
   } else {
-    result.innerHTML=`<p>A mensagem "${text}" decodificada é ${window.cipher.decode(offset, text)}!<p>`;
+    result.textContent=`<p>A mensagem "${text}" decodificada é ${window.cipher.decode(offset, text)}!<p>`;
   }
   form.reset();  
 }
